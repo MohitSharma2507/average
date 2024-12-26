f34019954cf075e42bd98b3a5f355b103a3958af
MohitSharma2507 <mohitsharma152531@gmail.com>
2024-12-26 19:41:16 +0530

    average.c

--- average.c
+++ average.c
@@ -5,7 +5,7 @@ int main()
     int size=5,a[size], sum = 0;
     float average;
 
-    printf("ENTER NUMBER\n");
+    printf("ENTER INPUT\n");
     for (int i = 0; i < size; i++)
     {
         scanf("%d", &a[i]);
